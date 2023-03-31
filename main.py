a2fc303ffe64222cd8ba00a9730160377e758192
Colinm1215 <colinm1215@gmail.com>
2023-03-31 11:22:38 -0400

    minimax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import copy
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -49,78 +51,84 @@ def print_board(board):
 
 
 def place_move(board, player, move_r, move_c):
+    game_board = copy.deepcopy(board)
     if move_r % 2 == 0:
         if move_c % 2 == 0:
             return "Invalid Move"
         else:
-            board[move_r][move_c] = "-"
+            game_board[move_r][move_c] = "-"
         row_above = move_r - 2
         row_below = move_r + 2
         if row_above >= 0:
-            if board[row_above][move_c] == "-":
+            if game_board[row_above][move_c] == "-":
                 col_left = move_c - 1
                 col_right = move_c + 1
                 if col_left >= 0:
-                    if board[move_r - 1][col_left] == "|":
-                        if col_right < len(board[move_r]):
-                            if board[move_r - 1][col_right] == "|":
-                                board[move_r - 1][move_c] = player
-        if row_below < len(board):
-            if board[row_below][move_c] == "-":
+                    if game_board[move_r - 1][col_left] == "|":
+                        if col_right < len(game_board[move_r]):
+                            if game_board[move_r - 1][col_right] == "|":
+                                game_board[move_r - 1][move_c] = player
+        if row_below < len(game_board):
+            if game_board[row_below][move_c] == "-":
                 col_left = move_c - 1
                 col_right = move_c + 1
                 if col_left >= 0:
-                    if board[move_r + 1][col_left] == "|":
-                        if col_right < len(board[move_r]):
-                            if board[move_r + 1][col_right] == "|":
-                                board[move_r + 1][move_c] = player
+                    if game_board[move_r + 1][col_left] == "|":
+                        if col_right < len(game_board[move_r]):
+                            if game_board[move_r + 1][col_right] == "|":
+                                game_board[move_r + 1][move_c] = player
     else:
         if move_c % 2 == 0:
-            board[move_r][move_c] = "|"
+            game_board[move_r][move_c] = "|"
         else:
             return "Invalid Move"
         col_left = move_c - 2
         col_right = move_c + 2
         if col_left >= 0:
-            if board[move_r][col_left] == "|":
+            if game_board[move_r][col_left] == "|":
                 row_above = move_r-1
                 row_below = move_r+1
                 if row_above >= 0:
-                    if board[row_above][move_c-1] == "-":
-                        if row_below < len(board):
-                            if board[row_below][move_c-1] == "-":
-                                board[move_r][move_c-1] = player
-        if col_right < len(board[move_r]):
-            if board[move_r][col_right] == "|":
+                    if game_board[row_above][move_c - 1] == "-":
+                        if row_below < len(game_board):
+                            if game_board[row_below][move_c - 1] == "-":
+                                game_board[move_r][move_c - 1] = player
+        if col_right < len(game_board[move_r]):
+            if game_board[move_r][col_right] == "|":
                 row_above = move_r-1
                 row_below = move_r+1
                 if row_above >= 0:
-                    if board[row_above][move_c+1] == "-":
-                        if row_below < len(board):
-                            if board[row_below][move_c+1] == "-":
-                                board[move_r][move_c+1] = player
+                    if game_board[row_above][move_c + 1] == "-":
+                        if row_below < len(game_board):
+                            if game_board[row_below][move_c + 1] == "-":
+                                game_board[move_r][move_c + 1] = player
+    return game_board
 
 # returns 0 if board not finished
 # returns 1 if player 1 wins
 # returns 2 if player 2 wins
 # returns 3 if a tie occurs
 def check_win(board):
-    size = len(board)
+    size = (len(board)-1)/2
     r = 1
     player1_count = 0
     player2_count = 0
-    while r < size:
-        c = 1
-        while c < size:
-            if board[r][c] == "1":
-                player1_count += 1
-            elif board[r][c] == "2":
-                player2_count += 1
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if (row % 2 != 0):
+                if (col % 2 != 0):
+                    if board[row][col] == "1":
+                        player1_count += 1
+                    elif board[row][col] == "2":
+                        player2_count += 1
             else:
-                return 0
-            c += 2
-        r += 2
-    if player2_count + player1_count != size - 1:
+                if (col % 2 == 0):
+                    if board[row][col] == "1":
+                        player1_count += 1
+                    elif board[row][col] == "2":
+                        player2_count += 1
+
+    if player2_count + player1_count != size*size:
         return 0
     else:
         if player1_count > player2_count:
@@ -131,7 +139,127 @@ def check_win(board):
             return 3
 
 
+def evaluate(board, player):
+    player1_count = 0
+    player2_count = 0
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if (row % 2 != 0):
+                if (col % 2 != 0):
+                    if board[row][col] == "1":
+                        player1_count += 1
+                    elif board[row][col] == "2":
+                        player2_count += 1
+            else:
+                if (col % 2 == 0):
+                    if board[row][col] == "1":
+                        player1_count += 1
+                    elif board[row][col] == "2":
+                        player2_count += 1
+
+    if player == "1":
+        return player1_count - player2_count
+    else:
+        return player2_count - player1_count
+
+
+def get_valid_moves(board):
+    validMoves = []
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if (row % 2 == 0):
+                if (col % 2 != 0):
+                    if board[row][col] == " ":
+                        validMoves.append([row,col])
+            else:
+                if (col % 2 == 0):
+                    if board[row][col] == " ":
+                        validMoves.append([row,col])
+    return validMoves
+
+
+def minimax(board, depth, player, isMaximizingPlayer, alpha, beta):
+    win_check = check_win(board)
+    if depth == 0 or (win_check == 1 or win_check == 2):
+        return evaluate(board, player)
+
+    if isMaximizingPlayer:
+        best_value = float('-inf')
+        for move in get_valid_moves(board):
+            new_board = place_move(board, player, move[0], move[1])
+            value = minimax(new_board, depth - 1, player, False, alpha, beta)
+            best_value = max(best_value, value)
+            alpha = max(alpha, best_value)
+            if beta <= alpha:
+                break
+        return best_value
+    else:
+        best_value = float('inf')
+        for move in get_valid_moves(board):
+            new_board = place_move(board, player, move[0], move[1])
+            value = minimax(new_board, depth - 1, player, True, alpha, beta)
+            best_value = min(best_value, value)
+            beta = min(beta, best_value)
+            if beta <= alpha:
+                break
+        return best_value
+
+
+def get_best_move(board, depth, player):
+    game_board = copy.deepcopy(board)
+    best_move = None
+    best_value = float('-inf')
+
+    move_list = get_valid_moves(game_board)
+
+    if len(move_list) == 1:
+        return move_list[0]
+
+    for move in move_list:
+        new_board = place_move(game_board, player, move[0], move[1])
+        value = minimax(new_board, depth - 1, player, True, float('-inf'), float('inf'))
+        if value > best_value:
+            best_value = value
+            best_move = move
+
+    return best_move
+
+
 if __name__ == '__main__':
-    board = read_gamestate("scenario1.txt")
+    inp = input("Please enter the name of the gamestate file to read in, or enter \"new gamestate\" : ")
+    board = []
+    if inp == "new gamestate":
+        size = int(input("Please enter the size of the new board : "))
+        board = create_gameboard(size)
+    else:
+        board = read_gamestate(inp)
+
+    depth = int(input("At what max_depth should the minimax algorithm search to find a move? : "))
+    done = False
+    turn = 1
+
     print_board(board)
-    print(check_win(board))
+
+    while not done:
+        if turn % 2 == 0:
+            player = "2"
+            print("Player 2's turn!")
+        else:
+            print("Player 1's turn!")
+            player = "1"
+        player_move = get_best_move(board, depth, player)
+        board = place_move(board, player, player_move[0], player_move[1])
+
+        print_board(board)
+        win_check = check_win(board)
+        if (win_check > 0):
+            done = True
+        if (win_check == 1):
+            print("Player 1 has won the game!")
+        elif (win_check == 2):
+            print("Player 2 has won the game!")
+        elif (win_check == 3):
+            print("The game has been tied!")
+        turn += 1
+
+
